76bb8604f824374666e5f630a14dd3fd95b8e22e
sam <79630413+ConradMcGrifter@users.noreply.github.com>
2021-04-19 15:57:58 -0400

    added social media icons to footer + javaScript stuff
    javaScript - made the page scroll back to top and close the main article if it is expanded

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const readMore = document.getElementById("newest-post__button");
 const hidden = document.getElementById("hidden");
+const backToTop = document.getElementById("backToTop");
 
 readMore.addEventListener("click", showArticle);
+backToTop.addEventListener("click", closeArticle);
 
 function showArticle() {
     hidden.classList.toggle("show");
@@ -10,4 +12,12 @@ function showArticle() {
         readMore.style.display = "none";
         hidden.style.animation = "fadeIn 1s";
     }
+}
+
+function closeArticle() {
+    if (hidden.classList.contains("show")) {
+        hidden.classList.toggle("show");
+    }
+    readMore.style.display = "block";
+    document.getElementById("header").scrollIntoView();
 }
