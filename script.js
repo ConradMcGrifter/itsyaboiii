1b97716106d4a98e2e77989b3ab945b66e3e4e47
sam <79630413+ConradMcGrifter@users.noreply.github.com>
2021-04-20 22:54:26 -0400

    added body overflow hidden when mobile navbar is revealed
    this prevents scrolling while the navbar is opened on mobile

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const hidden = document.getElementById("hidden");
 const readMore = document.getElementById("newest-post__button");
 const backToTop = document.getElementById("backToTop");
@@ -6,6 +5,7 @@ const hamburger = document.getElementById("hamburger");
 const navbar = document.getElementById("navbar");
 const navbarUl = document.getElementById("navbar__ul");
 const darken = document.getElementById("darken");
+const body = document.getElementById("body");
 
 readMore.addEventListener("click", showArticle);
 backToTop.addEventListener("click", closeArticle);
@@ -19,7 +19,6 @@ function showArticle() {
         readMore.style.display = "none";
         backToTop.style.display = "flex";
         hidden.style.animation = "fadeIn 1s";
-        
     }
 }
 
@@ -32,11 +31,14 @@ function closeArticle() {
     document.getElementById("header").scrollIntoView();
 }
 
-
 function sidebar() {
-hamburger.classList.toggle("reveal");
-navbar.classList.toggle("reveal");
-navbarUl.classList.toggle("reveal");
-darken.classList.toggle("darken");
-
+    hamburger.classList.toggle("reveal");
+    navbar.classList.toggle("reveal");
+    navbarUl.classList.toggle("reveal");
+    darken.classList.toggle("darken");
+    if (hamburger.classList.contains("reveal")) {
+        body.style.overflow = "hidden";
+    } else {
+        body.style.overflow = "";
+    }
 }
